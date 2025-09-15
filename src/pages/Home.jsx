7838b5cbabdb504912492ef08f24d660d3ddf81b
Eian100 <eian100@comcast.net>
2025-09-15 17:02:31 -0400

    Updated UI
    Navbar implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { StarBackground } from "@/components/StarBackground";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Navbar } from "@/components/Navbar";
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
